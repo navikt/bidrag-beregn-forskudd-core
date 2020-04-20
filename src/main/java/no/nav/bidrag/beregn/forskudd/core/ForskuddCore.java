d855ec8b1c4ad3edfa833ba369bfc460d1deb417
Tor Egil Jacobsen <tor.egil.jacobsen@nav.no>
2020-04-20 15:29:38 +0200

    impl klasse er ukjent for omverden

--- src/main/java/no/nav/bidrag/beregn/forskudd/core/ForskuddCore.java
+++ src/main/java/no/nav/bidrag/beregn/forskudd/core/ForskuddCore.java
@@ -2,8 +2,13 @@ package no.nav.bidrag.beregn.forskudd.core;
 
 import no.nav.bidrag.beregn.forskudd.core.dto.BeregnForskuddGrunnlagCore;
 import no.nav.bidrag.beregn.forskudd.core.dto.BeregnForskuddResultatCore;
+import no.nav.bidrag.beregn.forskudd.core.periode.ForskuddPeriode;
 
 public interface ForskuddCore {
 
   BeregnForskuddResultatCore beregnForskudd(BeregnForskuddGrunnlagCore grunnlag);
+
+  static ForskuddCore getInstance() {
+    return new ForskuddCoreImpl(ForskuddPeriode.getInstance());
+  }
 }
